ee1a8da85bd661d38213b2ec089463b168e4530f
tausifzmn <tausifzmn4@gmail.com>
2018-11-24 22:54:59 -0600

    12 mins

---
+++ app/src/main/java/com/example/tausi/everypad/adapters/NotesAdapter.java
@@ -0,0 +1,57 @@
+package com.example.tausi.everypad.adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.tausi.everypad.R;
+import com.example.tausi.everypad.model.Note;
+
+import java.util.ArrayList;
+
+public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NoteHolder>{
+
+    private ArrayList<Note> notes;
+
+    public NotesAdapter(ArrayList<Note> notes) {
+        this.notes = notes;
+    }
+
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_layout,parent, false);
+        return new NoteHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteHolder holder, int position) {
+        Note note = getNote(position);
+        if (note != null){
+            holder.noteText.setText(note.getNoteText());
+            holder.noteDate.setText();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    private Note getNote(int position){
+        return notes.get(position);
+    }
+
+    class NoteHolder extends RecyclerView.ViewHolder {
+        TextView noteText, noteDate;
+
+        public NoteHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+}
